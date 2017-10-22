fd99dd28e5808f81848b52cbe782a57b45acf7df
Ole <ole@vprsm.de>
2017-10-22 18:49:31 +0100

    Implement Category/Arrow/Profunctor approach to query building

--- src/Language/SQL/Builder.hs
+++ src/Language/SQL/Builder.hs
@@ -1,150 +1,142 @@
-{-# LANGUAGE DataKinds          #-}
-{-# LANGUAGE ExplicitNamespaces #-}
-{-# LANGUAGE FlexibleContexts   #-}
-{-# LANGUAGE FlexibleInstances  #-}
-{-# LANGUAGE GADTs              #-}
-{-# LANGUAGE TypeFamilies       #-}
-{-# LANGUAGE TypeOperators      #-}
-
 module Language.SQL.Builder (
-    Builder (..),
-    buildCode,
-    buildParams,
-
-    nest,
-    unnest,
-    isolateParams,
-    type (++),
-    append,
-    Function,
-    withParams
+    Builder,
+    hole,
+    code,
+    flatten
 ) where
 
-import           Control.Arrow
-import           Control.Monad.RWS.Strict
+import           Prelude          hiding (foldr, id, (.))
 
-import qualified Data.ByteString          as B
-import qualified Data.ByteString.UTF8     as UTF8
+import           Control.Arrow
+import           Control.Category
+import           Control.Monad
+import           Data.Profunctor
+
+import qualified Data.ByteString  as ByteString
+import           Data.Foldable
+import qualified Data.Sequence    as Seq
 import           Data.String
 
--- | Query builder
-data Builder ts p where
-    Code  :: B.ByteString         -> Builder ts p -> Builder ts       p
-    Param :: p                    -> Builder ts p -> Builder ts       p
-    Nest  :: Builder '[] (t -> p) -> Builder ts p -> Builder (t : ts) p
-    Nil   ::                                         Builder '[]      p
+-- | Builder element
+data Element input output
+    = Hole (input -> output)
+    | Code ByteString.ByteString
 
-instance Functor (Builder ts) where
-    fmap f segment =
-        case segment of
-            Code  code  rest -> Code  code              (f <$> rest)
-            Param param rest -> Param (f param)         (f <$> rest)
-            Nest  nest  rest -> Nest  (fmap f <$> nest) (f <$> rest)
-            Nil              -> Nil
+instance Functor (Element input) where
+    {-# INLINE fmap #-}
 
-instance Applicative (Builder '[]) where
-    pure x = Param x Nil
+    fmap mapOutput (Hole gen)  = Hole (mapOutput . gen)
+    fmap _         (Code code) = Code code
 
-    Code  code rest <*> rhs = Code code (rest <*> rhs)
-    Param f    rest <*> rhs = append (f <$> rhs) (rest <*> rhs)
-    Nil             <*> _   = Nil
+instance Profunctor Element where
+    {-# INLINE dimap #-}
 
-instance Show (Builder ts p) where
-    show builder =
-        concat (showBuilder builder)
+    dimap mapInput mapOutput (Hole gen)  = Hole (mapOutput . gen . mapInput)
+    dimap _        _         (Code code) = Code code
+
+    {-# INLINE lmap #-}
+
+    lmap mapInput (Hole gen)  = Hole (gen . mapInput)
+    lmap _        (Code code) = Code code
+
+    {-# INLINE rmap #-}
+
+    rmap = fmap
+
+-- | Transform the 'Hole', if it is one.
+mapHole
+    :: ((input -> output) -> (input' -> output'))
+    -> Element input output
+    -> Element input' output'
+mapHole trans (Hole gen)  = Hole (trans gen)
+mapHole _     (Code code) = Code code
+
+-- | Builder
+newtype Builder input output = Builder { unBuilder :: Seq.Seq (Element input output) }
+
+instance Monoid (Builder input output) where
+    {-# INLINE mempty #-}
+
+    mempty = Builder Seq.empty
+
+    {-# INLINE mappend #-}
+
+    mappend (Builder lhs) (Builder rhs) = Builder (lhs Seq.>< rhs)
+
+    {-# INLINE mconcat #-}
+
+    mconcat builders = Builder (mconcat (unBuilder <$> builders))
+
+instance Functor (Builder input) where
+    {-# INLINE fmap #-}
+
+    fmap mapOutput (Builder elements) = Builder (fmap mapOutput <$> elements)
+
+instance Profunctor Builder where
+    {-# INLINE dimap #-}
+
+    dimap mapInput mapOutput (Builder elements) = Builder (dimap mapInput mapOutput <$> elements)
+
+    {-# INLINE lmap #-}
+
+    lmap mapInput (Builder elements) = Builder (lmap mapInput <$> elements)
+
+    {-# INLINE rmap #-}
+
+    rmap mapOutput (Builder elements) = Builder (rmap mapOutput <$> elements)
+
+instance Category Builder where
+    id = Builder (Seq.singleton (Hole id))
+
+    Builder lhs . Builder rhs =
+        Builder (join (fillHole <$> lhs))
         where
-            showBuilder :: Builder ts p -> [String]
-            showBuilder segment =
-                case segment of
-                    Code  code  rest -> UTF8.toString code : showBuilder rest
-                    Param _     rest -> "<placeholder>" : showBuilder rest
-                    Nest  nest  rest -> showBuilder nest ++ showBuilder rest
-                    Nil              -> []
+            fillHole (Hole gen)  = fmap gen <$> rhs
+            fillHole (Code code) = Seq.singleton (Code code)
 
-instance IsString (Builder '[] p) where
-    fromString str = Code (UTF8.fromString str) Nil
+instance Arrow Builder where
+    arr = hole
 
-instance Monoid (Builder '[] p) where
-    mempty = Code B.empty Nil
+    first = mapHoles first
 
-    mappend = append
+    second = mapHoles second
 
--- | Unary version of 'Nest'.
-nest :: Builder '[] (t -> p) -> Builder '[t] p
-nest x = Nest x Nil
+instance IsString (Builder input output) where
+    fromString str = code (fromString str)
 
--- | Dual of 'nest'.
-unnest :: Builder (t : ts) p -> Builder ts (t -> p)
-unnest segment =
-    case segment of
-        Code  code  rest -> Code code (unnest rest)
-        Param param rest -> Param (const param) (unnest rest)
-        Nest  nest  rest -> append nest (const <$> rest)
+-- | Map all the 'Hole's inside the 'Builder'.
+mapHoles
+    :: ((input -> output) -> (input' -> output'))
+    -> Builder input output
+    -> Builder input' output'
+mapHoles trans (Builder lhs) = Builder (mapHole trans <$> lhs)
 
--- | Remove code segments from a 'Builder'.
-isolateParams :: Builder ts p -> Builder ts p
-isolateParams segment =
-    case segment of
-        Code  _     rest -> isolateParams rest
-        Param param rest -> Param param (isolateParams rest)
-        Nest  nest  rest -> Nest (isolateParams nest) (isolateParams rest)
-        Nil              -> Nil
+-- | Produce a 'Builder' that only consists of a 'Hole'.
+hole :: (input -> output) -> Builder input output
+hole gen = Builder (Seq.singleton (Hole gen))
 
--- | Append two type lists.
-type family (++) ts us where
-    (++) '[]      us  = us
-    (++) (t : ts) us  = t : ts ++ us
+-- | Produce a 'Builder' that only consists of 'Code'.
+code :: ByteString.ByteString -> Builder input output
+code code = Builder (Seq.singleton (Code code))
 
--- | Append 'two' builders.
-append :: Builder ts p -> Builder us p -> Builder (ts ++ us) p
-append segment rhs =
-    case segment of
-        Code  code  lhs -> Code  code  (append lhs rhs)
-        Param param lhs -> Param param (append lhs rhs)
-        Nest nest  lhs  -> Nest  nest  (append lhs rhs)
-        Nil             -> rhs
+-- | Flatten 'Builder'.
+flatten
+    :: (Word -> (input -> output) -> ByteString.ByteString)
+    -> Builder input output
+    -> (ByteString.ByteString, [input -> output])
+flatten holeCode (Builder elements) =
+    (code, gens)
+    where
+        (_, code, gens) = foldr f (0, ByteString.empty, []) elements
 
--- | @Function '[p1, p2, ... pn] r@ constructs a type signature @p1 -> p2 -> ... pn -> r@.
-type family Function ts r where
-    Function '[]      r = r
-    Function (t : ts) r = t -> Function ts r
+        f elem (index, codes, gens) =
+            case elem of
+                Hole gen ->
+                    ( index + 1
+                    , ByteString.append (holeCode index gen) codes
+                    , gen : gens )
 
--- | Do something with parameters inside the builder.
-withParams :: ([p] -> r) -> Builder ts p -> Function ts r
-withParams ret segment =
-    case segment of
-        Code  _     rest -> withParams ret               rest
-        Param value rest -> withParams (ret . (value :)) rest
-        Nest  nest  rest -> \ param -> withParams (ret . (buildParams (($ param) <$> nest) ++)) rest
-        Nil              -> ret []
-
--- | Reader-writer-state-transformer which gathers collects code segments.
-buildCodeSegments :: Builder ts p -> RWS (Word -> B.ByteString) B.ByteString Word ()
-buildCodeSegments segment =
-    case segment of
-        Code code rest -> do
-            tell code
-            buildCodeSegments rest
-
-        Param _ rest -> do
-            index <- state (id &&& (+ 1))
-            asks ($ index) >>= tell
-            buildCodeSegments rest
-
-        Nest nest rest -> do
-            buildCodeSegments nest
-            buildCodeSegments rest
-
-        Nil -> pure ()
-
--- | @buildCode placeholderCode builder@ collects the code segments from a 'Builder'. It also
--- translates 'Param's to placeholders using the given @placeholderCode@. The parameter to
--- @placeholderCode@ is the parameter index. Indices start at 0.
-buildCode :: (Word -> B.ByteString) -> Builder ts p -> B.ByteString
-buildCode placeholder segment =
-    snd (evalRWS (buildCodeSegments segment) placeholder 0)
-
--- | @buildParams sql@ produces a function which collects all the necessary parameters for the
--- query described in @sql@.
-buildParams :: Builder ts p -> Function ts [p]
-buildParams = withParams id
+                Code code ->
+                    ( index
+                    , ByteString.append code codes
+                    , gens )
