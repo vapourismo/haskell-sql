d06f426073f03ce101079e4787f8e55a06cd47e7
Ole <ole@vprsm.de>
2017-11-04 14:17:10 +0000

    Reintroduce the quasi-quoter for the Builder only

---
+++ src/Language/SQL/TH.hs
@@ -0,0 +1,134 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Language.SQL.TH (
+    sql
+) where
+
+import           Control.Applicative
+import           Control.Monad.Trans
+
+import qualified Codec.Binary.UTF8.String    as UTF8
+import qualified Data.ByteString             as B
+import           Data.Char
+import           Data.List
+import           Text.Parsec                 hiding (many, (<|>))
+
+import           Language.Haskell.Meta.Parse
+import           Language.Haskell.TH
+import           Language.Haskell.TH.Quote
+import qualified Language.Haskell.TH.Syntax  as TH
+
+import           Language.SQL.Builder
+
+type Parser = ParsecT String () Q
+
+-- | Generate a 'B.ByteString' expression using the given 'String'.
+liftAsByteString :: String -> Q Exp
+liftAsByteString str =
+    [e| B.pack $(TH.lift (UTF8.encode str)) |]
+
+-- | Generate a 'Code' expression.
+toCodeExp :: String -> Parser Exp
+toCodeExp strCode =
+    AppE (VarE 'code) <$> lift (liftAsByteString strCode)
+
+-- | Something in parentheses
+inParentheses :: Parser String
+inParentheses = do
+    char '('
+    body <- many $ choice [ inParentheses
+                          , quotation '"'
+                          , quotation '\''
+                          , some (noneOf "'\"()") ]
+    char ')'
+    pure ('(' : concat body ++ ")")
+
+-- | Quotation
+quotation :: Char -> Parser String
+quotation delim = do
+    char delim
+    body <- many (escaped <|> pure <$> noneOf [delim])
+    char delim
+    pure (delim : concat body ++ [delim])
+    where
+        escaped = (\ a b -> [a, b]) <$> char '\\' <*> anyChar
+
+-- | Name pattern
+valueName :: Parser Name
+valueName = do
+    strName <- (:) <$> (letter <|> char '_') <*> many (alphaNum <|> char '_')
+    lift $ do
+        mbName <- lookupValueName strName
+        case mbName of
+            Nothing      -> fail ("Name " ++ show strName ++ " does not refer to a value")
+            Just valName -> pure valName
+
+-- | Arrow code
+arrowCode :: Parser Exp
+arrowCode = do
+    char '#'
+    body <- inParentheses
+    either (lift . fail) (pure . AppE (VarE 'hole)) (parseExp body)
+
+-- | Arrow name
+arrowName :: Parser Exp
+arrowName = do
+    char '#'
+    AppE (VarE 'hole) . VarE <$> valueName
+
+-- | Inline code
+inlineCode :: Parser Exp
+inlineCode = do
+    char '$'
+    body <- inParentheses
+    either (lift . fail) pure (parseExp body)
+
+-- | Inline name
+inlineName :: Parser Exp
+inlineName = do
+    char '$'
+    VarE <$> valueName
+
+-- | Parser for SQL code
+sqlCode :: Parser Exp
+sqlCode = do
+    segments <- many $
+        choice [ quotation '"' >>= toCodeExp
+               , quotation '\'' >>= toCodeExp
+               , try arrowCode
+               , arrowName
+               , try inlineCode
+               , inlineName
+               , some (noneOf "\"'#$") >>= toCodeExp ]
+    lift [e| mconcat $(pure (ListE segments)) |]
+
+-- | Parse SQL code and generate a 'SQL' expression from it.
+parseSqlCode :: String -> Q Exp
+parseSqlCode inputCode = do
+    result <- runParserT (sqlCode <* eof) () "quasi-quote"
+                         (dropWhileEnd isSpace (dropWhile isSpace inputCode))
+    case result of
+        Left parseError -> do
+            Loc _ _ _ (lineStart, charStart) _ <- location
+            let sourcePos = errorPos parseError
+                lineReal  = lineStart + sourceLine sourcePos - 1
+                charReal  = charStart + sourceColumn sourcePos - 1
+
+            fail $
+                "(line "
+                ++ show lineReal
+                ++ ", column "
+                ++ show charReal
+                ++ "): "
+                ++ intercalate ", " (tail (lines (show parseError)))
+
+        Right result ->
+            pure result
+
+-- | Basic SQL quasi-quoter
+sql :: QuasiQuoter
+sql =
+    QuasiQuoter { quoteExp  = parseSqlCode
+                , quoteDec  = const (error "'sql' quasi-quoter works only in expressions")
+                , quotePat  = const (error "'sql' quasi-quoter works only in expressions")
+                , quoteType = const (error "'sql' quasi-quoter works only in expressions") }
